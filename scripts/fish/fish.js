b0501c0b898804c04a8be206f738c5f12749dece
Michael Baumer <meeesterbaumer@gmail.com>
2020-08-14 11:10:20 -0500

    made cards do the flippy dance

--- scripts/fish/fish.js
+++ scripts/fish/fish.js
@@ -2,13 +2,19 @@
 
 export const fishAsHTML = (fishObj) => {
   return `
-  <section class="fish">
-  <img src="${fishObj.image}" class="fish__image" alt="">
-  <p class="fish__name">${fishObj.name}</p>
-  <p class="fish__species">${fishObj.species}</p>
-  <p class="fish__length">${fishObj.length}</p>
-  <p class="fish__location">${fishObj.location}</p>
-  <p class="fish__diet">${fishObj.food}</p>
-  </section>
+  <div class="flip-card">
+    <div class="flip-card-inner">
+      <div class="flip-card-front fish">
+      <img src="${fishObj.image}" style="width:200px;height:200px;" class="fish__image" alt="">
+      </div>
+      <div class="flip-card-back">
+        <p class="fish__name">Name: ${fishObj.name}</p>
+        <p class="fish__species">Species: ${fishObj.species}</p>
+        <p class="fish__length">length: ${fishObj.length}</p>
+        <p class="fish__location">Captured: ${fishObj.location}</p>
+        <p class="fish__diet">Diet: ${fishObj.food}</p>
+      </div>
+    </div>
+  </div>
   `
 }
